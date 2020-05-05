41d10a986bc0ac6805d50c551e0204883cbbfda7
Mohamed Beshri Amer <mohamed.perry351@gmail.com>
2020-05-04 22:41:04 -0700

    This my 2nd Commit in this project

---
+++ Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookDownloader.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string BookName { get; set; }
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BookDownloader
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("Default",
+                    "{controller}/{action}/{id?}",
+                    new { controller = "Home", action = "Index" });
+
+            });
+        }
+    }
+}
